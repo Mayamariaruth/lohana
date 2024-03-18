886f7b21b98ff05280e528116c0b6bc990cff370
Maya Thompson <mayathompson@Mayas-MacBook-Air.local>
2024-03-18 15:24:02 +0700

    add images to products and customize admin products panel

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,17 @@ from .models import Product, Category
 
 
 # Register your models here.
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'category',
+        'sku',
+        'name',
+        'price',
+        'image',
+    )
+
+    ordering = ('sku', 'category', 'name',)
+
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Category)
