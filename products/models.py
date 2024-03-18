da4807371c5c57fa124bb8cd2e654f1db4058d96
Maya Thompson <mayathompson@Mayas-MacBook-Air.local>
2024-03-18 16:51:19 +0700

    add product introduction field to products model and in template

--- products/models.py
+++ products/models.py
@@ -16,6 +16,7 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
+    introduction = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
