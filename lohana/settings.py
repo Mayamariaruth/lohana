f98ca5f9b340669b521fb2cd01ff1d0411070b28
Maya Thompson <mayathompson@Mayas-MacBook-Air.local>
2024-03-20 23:24:04 +0700

    create toast messages and basic styling

--- lohana/settings.py
+++ lohana/settings.py
@@ -7,7 +7,6 @@ if os.path.isfile('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -70,6 +69,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 AUTHENTICATION_BACKENDS = (
     # Needed to login by username in Django admin, regardless of `allauth`
