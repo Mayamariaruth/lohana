bb66a2be1f80037079c86eb4b572b4123574f6c4
Maya Thompson <mayathompson@Mayas-MacBook-Air.local>
2024-03-25 13:11:31 +0700

    update checkout form for country field

--- checkout/forms.py
+++ checkout/forms.py
@@ -21,23 +21,21 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
-            'county': 'County',
+            'county': 'County / State',
         }
 
-        # Set autofocus on the first field
         self.fields['full_name'].widget.attrs['autofocus'] = True
         
-        # Iterate over fields to set placeholders, classes, and remove labels
         for field_name, field in self.fields.items():
-            if field.required:
-                placeholder = f'{placeholders[field_name]} *'
-            else:
-                placeholder = placeholders[field_name]
-            field.widget.attrs['placeholder'] = placeholder
+            if field_name != 'country': 
+                if field.required:
+                    placeholder = f'{placeholders[field_name]} *'
+                else:
+                    placeholder = placeholders[field_name]
+                field.widget.attrs['placeholder'] = placeholder
             field.widget.attrs['class'] = 'stripe-style-input'
             field.label = False
