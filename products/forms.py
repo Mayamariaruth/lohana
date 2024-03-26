97078aaed324e10025db445bed8e019931b25ad5
Maya Thompson <mayathompson@172-15-65-224.lightspeed.miamfl.sbcglobal.net>
2024-03-26 18:49:41 +0700

    create widget for image field

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django_summernote.widgets import SummernoteWidget
+from .widgets import CustomClearableFileInput
 from .models import Product
 
 
@@ -9,6 +10,8 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['description'].widget = SummernoteWidget()
