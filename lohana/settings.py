5102021097989535ccd2ecba81440af8653a7a32
Maya Thompson <mayathompson@Mayas-MacBook-Air.local>
2024-03-27 16:56:14 +0800

    change location of custom_storages.py again and add new path in settings.py file for deployment

--- lohana/settings.py
+++ lohana/settings.py
@@ -171,10 +171,10 @@ if 'USE_AWS' in os.environ:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
+    STATICFILES_STORAGE = 'home.custom_storages.StaticStorage'
+    DEFAULT_FILE_STORAGE = 'home.custom_storages.MediaStorage'
 
     # Override static and media URLs in production
     STATIC_URL = F'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
