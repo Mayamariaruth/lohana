8932dbdb242e81393c14a64d49425d41ad71911f
Maya Thompson <mayathompson@Mayas-MacBook-Air.local>
2024-03-27 15:52:09 +0800

    remove rating field from product model and move storages file to lohana for deployment

--- products/models.py
+++ products/models.py
@@ -20,7 +20,6 @@ class Product(models.Model):
     introduction = models.CharField(max_length=200, null=True, blank=True)
     description = SummernoteTextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
